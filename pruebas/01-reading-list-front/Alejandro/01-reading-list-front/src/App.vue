d8576e0392b2ba0a585bc2635a6a5dd81660d9b6
alejandrojurado28 <alejandrojuradomagdaleno@gmail.com>
2024-02-09 19:03:38 +0100

    Mostrando los libros e implementando la lógica de filtrar por género

--- pruebas/01-reading-list-front/Alejandro/01-reading-list-front/src/App.vue
+++ pruebas/01-reading-list-front/Alejandro/01-reading-list-front/src/App.vue
@@ -1,47 +1,45 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import 'tailwindcss/tailwind.css';
+  import booksData from './assets/books.json';
+  import FilterGenre from './components/FilterGenre.vue';
+  import { ref, computed } from 'vue';
+
+  const books = booksData.library;
+  const selectedGenre = ref('');
+
+  const filteredBooks = computed(() => {
+    if (selectedGenre.value === '') {
+      return books;
+    } else {
+      return books.filter(book => book.book.genre === selectedGenre.value);
+    }
+  });
+
+  const handleGenreFilter = (genre) => {
+    selectedGenre.value = genre;
+  };
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="bg-black p-8">
+    <h1 class="font-comic-sans text-white">8 Libros disponibles</h1>
+    <div>
+      <GenreFilter @genreFilter="handleGenrerFilter"></GenreFilter>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="grid grid-cols-3 gap-4 mt-4">
+      <div v-for="(book, index) in books" :key="index" class="p-4 border border-gray-300 rounded-lg">
+        <img :src="book.book.cover" alt="Portada del libro" class="mx-auto mb-4 w-40 h-56 object-cover">
+        <h2 class="text-xl font-semibold text-white">Título: {{ book.book.title }}</h2>
+        <p class="text-gray-600 text-white">Género: {{ book.book.genre }}</p>
+        <p class="mt-2 text-white">Autor: {{ book.book.author.name }}</p>
+        <p class="text-white">Páginas: {{ book.book.pages }}</p>
+        <p class="text-white">Año: {{ book.book.year }}</p>
+        <p class="text-white">ISBN: {{ book.book.ISBN }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+  @import url('https://fonts.googleapis.com/css2?family=Comic+Sans+MS&display=swap');
 </style>
---
+++ pruebas/01-reading-list-front/Alejandro/01-reading-list-front/src/components/FilterGenre.vue
@@ -0,0 +1,30 @@
+<script setup lang=ts>
+    import { ref, computed, defineEmits } from 'vue';
+    import booksData from '../assets/books.json';
+
+    const selectedGenre = ref('');
+    const books = booksData.library;
+
+    const genres = computed(() => {
+        const allGenres = books.map(book => book.book.genre);
+        return [...new Set(allGenres)]; // Eliminar duplicados
+    })
+
+    const emit = defineEmits(['genreFilter']);
+
+    const emitGenderFilter = () => {
+        emit('genreFilter', selectedGenre.value);
+    }
+</script>
+
+<template>
+    <p>Filtrar por género</p>
+    <select v-model="selectedGenre" class="px-4 py-2 rounded-lg bg-white text-gray-800" name="genero" id="genero">
+        <option value="">Todos</option>
+        <option v-for="genre in genres" :key="genre">{{ genre }}</option>
+    </select>
+</template>
+
+<style scoped>
+
+</style>
